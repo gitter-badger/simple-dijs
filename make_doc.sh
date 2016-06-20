bf751ccf7e0cbcd1189b519093908a0c8bbc6a88
Aubin VIGHIER <avighier@users.noreply.github.com>
2016-06-20 01:54:05 -0500

    Updated doc template

--- make_doc.sh
+++ make_doc.sh
@@ -3,3 +3,9 @@
 rm -R ./api-doc 2>/dev/null
 git archive master api_doc | tar xf -
 mv api_doc api-doc
+rm api-doc/index.html
+
+SED_EXPR='s#<a href="index.html">Home</a>#<a href="..">\&laquo; Back to project</a>#'
+
+sed -i "$SED_EXPR" api-doc/Di.html
+sed -i "$SED_EXPR" api-doc/di.js.html
